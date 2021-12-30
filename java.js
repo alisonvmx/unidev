ba83c74d894382803b8b4f4fde1ce4f2dffdee61
alisonvmx <85261428+alisonvmx@users.noreply.github.com>
2021-12-29 21:13:56 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,23 @@
+
+const bt = document.getElementById('bt');
+
+
+function limpar () {
+    var text = document.getElementById('t');
+    var value = text.value.split(" ");
+    var xings = ['merda', 'porra', 'caralho', 'desgrama', 'desgraça','fuder', 'puta', 'puto', 'corno', 'corna', 'desgraçado', 'miseravel'];
+    
+    for( var i=0; i<value.length;i++){
+        for( var j=0; j<xings.length;j++)
+        if(value[i]==xings[j]){
+        value[i]='****';
+     }
+    }
+    for( var i=0; i<value.length;i++){
+    console.log(value[i]);
+    }
+
+    value.textcontent = value;
+}
+
+bt.addEventListener ('click', limpar);
